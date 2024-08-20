ce544c02b433909f0199be19300c83dc2b5a0f53
Haewon <ihaewon488@gmail.com>
2024-08-20 12:37:56 +0900

    Added some pages and items needed

---
+++ constants/icons.ts
@@ -0,0 +1,41 @@
+import back from "../assets/icons/back.png";
+import bell from "../assets/icons/bell.png";
+import bill from "../assets/icons/bill.png";
+import close from "../assets/icons/close.png";
+import disable_eye from "../assets/icons/disable_eye.png";
+import down from "../assets/icons/down.png";
+import eye from "../assets/icons/eye.png";
+import game from "../assets/icons/games.png";
+import barcode from "../assets/icons/barcode.png";
+import info from "../assets/icons/info.png";
+import internet from "../assets/icons/internet.png";
+import more from "../assets/icons/more.png";
+import phone from "../assets/icons/phone.png";
+import reload from "../assets/icons/reload.png";
+import scan from "../assets/icons/scan.png";
+import send from "../assets/icons/send.png";
+import user from "../assets/icons/user.png";
+import wallet from "../assets/icons/wallet.png";
+
+const icons = {
+    back: require("../assets/icons/back.png"),
+    bell: require("../assets/icons/bell.png"),
+    bill,
+    close: require("../assets/icons/close.png"),
+    disable_eye: require("../assets/icons/disable_eye.png"),
+    down: require("../assets/icons/down.png"),
+    eye: require("../assets/icons/eye.png"),
+    game,
+    barcode: require("..assets/icons/barcode.png"),
+    info: require("../assets/icons/info.png"),
+    internet,
+    more,
+    phone: require("..assets/icons/phone.png"),
+    reload,
+    scan,
+    send,
+    user,
+    wallet,
+} as const;
+
+export default icons;
---
+++ constants/images.ts
@@ -0,0 +1,18 @@
+import wallieLogo from"../assets/images/wallie-logo.png";
+import banner from "../assets/images/banner.png";
+import promoBanner from"../assets/images/promo-banner.png";import focus from"../assets/images/focus.png";
+
+// Dummy
+import usFlag from "../assets/images/us-flag.jpg";
+import InFlag from "../assets/images/IN.png";
+
+export default {
+    wallieLogo: require("../assets/images/wallie-logo.png"),
+    banner,
+    promoBanner,
+    focus: require("..assets/images/focus.png"),
+
+    // Dummy
+    usFlag,
+    InFlag
+}
